89ff0301b1bb038ab0620b30309b15fa56b429be
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-07-08 02:27:42 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE books(
 id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
 
 CREATE TABLE characters(
-id PRIMARY KEY, name TEXT, species TEXT, motto TEXT, author_id INTEGER)
+id PRIMARY KEY, name TEXT, species TEXT, motto TEXT, author_id INTEGER);
 
 CREATE TABLE character_books (
 id PRIMARY KEY, book_id INTEGER, character_id INTEGER);
